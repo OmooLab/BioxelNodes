cd00c2bfef24f6b1d512f4fc6fde3df36df2530d
nan <icrdr2010@outlook.com>
2026-05-25 14:09:44 +0800

    feat: Update release workflow and manifest for improved dependency management and layer handling

--- src/bioxelnodes/constants.py
+++ src/bioxelnodes/constants.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 NODE_LIB_DIRPATH = Path(Path(__file__).parent,
-                        "assets/O Bioxel").resolve()
+                        "assets/O_Bioxel").resolve()
 
 LATEST_NODE_LIB_PATH = Path(NODE_LIB_DIRPATH,
                             "BioxelNodes.blend").resolve()
