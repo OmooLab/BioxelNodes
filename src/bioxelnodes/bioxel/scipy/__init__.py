35faebb911f6b032d6c99d250aadb18fcfb88d56
nan <icrdr2010@outlook.com>
2026-07-07 13:07:54 +0800

    Remove deprecated modules and functions from bioxel package
    - Deleted _ni_support.py, _utils.py, and dtype.py as they contained unused or redundant functions.
    - Removed the resize function and related utilities from _warps.py to streamline image processing.
    - Cleaned up the skimage module by removing unnecessary imports and functions.

--- src/bioxelnodes/bioxel/scipy/__init__.py
+++
@@ -1,5 +0,0 @@
-from ._interpolation import zoom
-from ._filters import (gaussian_filter,
-                       median_filter,
-                       maximum_filter,
-                       minimum_filter)
